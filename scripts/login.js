4b60f4da21e9ca8f2efb0439df892eaf938d3e30
Md. Raju Ahmed Rifat <mrarifat295@gmail.com>
2026-03-17 16:11:27 +0600

    login mechanism, creating home page header

---
+++ scripts/login.js
@@ -0,0 +1,18 @@
+const loginBtn = document.getElementById('login-btn');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+
+
+loginBtn.addEventListener('click',function(){
+    const userValue = username.value;
+    const passValue = password.value;
+
+    if(userValue == 'admin' && passValue == 'admin123'){
+        window.location.assign('/home.html');
+    }
+    else{
+        alert('Wrong Credential :(')
+    }
+    username.value = '';
+    password.value = '';
+})
